5431d3bb41c40df1c41cce673ca77bf7187a2754
Sepideh Attar <sepideh.att@gmail.com>
2022-08-28 13:23:35 +0430

    Added location model

---
+++ src/location/location.model.ts
@@ -0,0 +1,12 @@
+import { Schema } from 'mongoose';
+
+export const LocationSchema = new Schema({
+  latitude : Number,
+  langitude: Number
+})
+
+
+export interface Location {
+  latitude : number,
+  langitude: number
+}
