b39727836bae63f5c5d31b4c71b586d4dd0fb16c
Sepideh Attar <sepideh.att@gmail.com>
2022-08-28 13:51:01 +0430

    added post req to create new location in DB

--- src/location/locations.controller.ts
+++ src/location/locations.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post,Body } from "@nestjs/common";
+import { LocationsService } from "./locations.service";
 
 
 @Controller('create-location')
 export class LocationsController {
+  constructor(private readonly locationService: LocationsService) { }
+
+  @Post()
+  async addProduct(
+    @Body('latitude') lat: number,
+    @Body('langitude') lang: number
+  ) {
+    const generatedId = await this.locationService.createLocation(lat,lang);
+    return { id: generatedId }
+  }
 
 }
--- src/location/locations.service.ts
+++ src/location/locations.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Location } from "./location.model";
 
 @Injectable()
 
 export class LocationsService {
+  constructor(@InjectModel('Location') private readonly locationModel: Model<Location>) { }
+
+  async createLocation(latitude: number, langitude: number) {
+    const newLocation = await this.locationModel.create({ latitude,langitude});
+    return newLocation.id;
+  };
 
 }
