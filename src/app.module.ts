26ff4d0f697d1e871d0e6d17e7ab656a0e048dc6
Sepideh Attar <sepideh.att@gmail.com>
2022-08-28 17:41:44 +0430

    Updated imports with user&auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationModule } from './location/locations.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports:[ConfigModule.forRoot({isGlobal: true}),LocationModule, MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LocationModule, MongooseModule.forRoot("mongodb://localhost:27017" || process.env.MONGO_URI), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
