126227bf8a0b335872c78e7e09b756355b7d1ed0
Sepideh Attar <sepideh.att@gmail.com>
2022-08-28 13:29:04 +0430

    Updated app.module-import mongoos&location modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LocationModule } from './location/locations.module';
 
 @Module({
-  imports: [],
+  imports: [LocationModule, MongooseModule.forRoot(
+    "mongodb+srv://sepidehatt:sepidehatt@cluster0.g9coksr.mongodb.net/nestjs-project?retryWrites=true&w=majority",
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
