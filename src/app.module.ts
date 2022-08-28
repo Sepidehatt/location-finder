96519a1e2b994c79c4c7fedbbdefef06104ed37a
Sepideh Attar <sepideh.att@gmail.com>
2022-08-28 14:35:02 +0430

    added env variable for mongodb URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationModule } from './location/locations.module';
 
+
 @Module({
-  imports: [LocationModule, MongooseModule.forRoot(
-    "mongodb+srv://sepidehatt:sepidehatt@cluster0.g9coksr.mongodb.net/nestjs-project?retryWrites=true&w=majority",
-  )],
+  imports:[ConfigModule.forRoot({isGlobal: true}),LocationModule, MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
